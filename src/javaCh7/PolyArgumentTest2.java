41e3e7cc218c251de638f8df613660f9f772e646
wonseopK <wonseop77@daum.net>
2021-12-31 10:40:30 +0900

    ch7 java abstract class study

---
+++ src/javaCh7/PolyArgumentTest2.java
@@ -0,0 +1,85 @@
+package javaCh7;
+
+class Product2 {
+	int price;
+	int bonusPoint;
+	
+	Product2(int price){
+		this.price = price;
+		bonusPoint = (int)(price/10.0);
+	}
+	
+	Product2(){};
+}
+
+class Tv2 extends Product {
+	Tv2() {
+		super(100);
+	}
+	public String toString() {
+		return "tv";
+	}
+}
+
+class Computer2 extends Product {
+	Computer2(){
+		super(200);
+	}
+	public String toString() {
+		return "Computer";
+	}
+}
+
+class Audio extends Product {
+	Audio() {
+		super(50);
+	}
+	public String toString() {
+		return "Audio";
+	}
+}
+
+class Buyer2 {
+	int money = 1000;
+	int bonusPoint = 0;
+	Product[] item = new Product[10];
+	int i = 0;
+	
+	void buy(Product p) {
+		if(money < p.price) {
+			System.out.println("잔액이 부족하여 물건을 살 수 없습니다.");
+			return;
+		}
+		
+		money -= p.price;
+		bonusPoint += p.bonusPoint;
+		item[i++] = p;
+		System.out.println(p + "을/를 구입하셨습니다.");
+	}
+	
+	void summary( ) {
+		int sum = 0;
+		String itemList = "";
+		
+		for(int i=0; i<item.length; i++) {
+			if(item[i] == null) {
+				break;
+			}
+			sum += item[i].price;
+			itemList += item[i] + ",";
+		}
+		System.out.println("구입하신 물품의 총금액은 " + sum + "만원입니다.");
+		System.out.println("구입하신 제품은 " + itemList + "입니다.");
+	}
+}
+
+public class PolyArgumentTest2 {
+	public static void main(String[] args) {
+		Buyer2 b = new Buyer2();
+		
+		b.buy(new Tv2());
+		b.buy(new Computer2());
+		b.buy(new Audio());
+		b.summary();
+	}
+}
